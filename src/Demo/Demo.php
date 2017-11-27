4d8518cdbb31b4852b9d4f6d869dd7de47a36b98
IMDem <568089002@qq.com>
2017-11-27 16:20:44 +0800

    first commit

---
+++ src/Demo/Demo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imden
+ * Date: 2017/11/27
+ * Time: 下午4:07
+ */
+namespace DemonAndAngel\Demo;
+class Demo {
+    public function say(){
+        echo "Hello World\n";
+    }
+}
